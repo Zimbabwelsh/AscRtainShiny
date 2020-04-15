59fe89dffe3c2716188001c1a99170c7608083e0
Gareth <garethjgriffith1@gmail.com>
2020-04-15 17:17:10 +0100

    Adding initial text, and reordering sliders

--- AscRtainshiny.R
+++ AscRtainshiny.R
@@ -6,32 +6,50 @@ library(shiny)
 library(AscRtain)
 
 
-ui <- fluidPage( "AscRtain Functionality",
-                 sliderInput(inputId="num1", label = "Observed OR",
-                             value=2, min=0, max=10),
-                 sliderInput(inputId="num2", label = "P(S=1)", 
-                             value = 0.0275, min=0.0001, max=1),
-                 sliderInput(inputId="num3", label = "P(A=1)",
-                             value = 0.15, min= 0.0001, max=1),
-                 sliderInput(inputId = "num4",label = "P(Y=1)",
-                             value = 0.1, min= 0.0001, max=1),
-                 sliderInput(inputId = "num5", label = "P(A=1 & Y=1)",
-                             value = 0, min=0, max=0),
-                 sliderInput(inputId = "num6", label = "b0_range",
-                             value=c(0,1), min=0, max=1),
-                 sliderInput(inputId = "num7", label = "ba_range",
-                             value=c(-0.2,0.2), min=-0.5, max=0.5),
-                 sliderInput(inputId = "num8", label = "by_range", 
-                             value=c(-0.2,0.2), min=-0.5, max=0.5),
-                 sliderInput(inputId = "num9", label = "bay_range",
-                             value= c(0,0), min=0, max=0),
-                 sliderInput(inputId="num10", label = "Granularity", 
-                             value=100, min=10, max=200),
+ui <- fluidPage( 
+  
+  titlePanel("AscRtain Functionality"),
+  
+  fluidRow(
+    column(3, p("This shiny app demonstrates some of the functional",
+                       "utility of the", strong("AscRtain"), " R Package. The function",
+                       "here explores the possible parameter space which",
+                       "could give rise to an observed OR between exposure",
+                       "and outcome. This plots the possible selection effects",
+                       "(entered as BA and BY) which could give rise to an",
+                       "observed OR under a true known OR of 1.")),
+    column(3, h4("Observed Relationship"),
+           sliderInput(inputId="num1", label = "Observed OR",
+                       value=2, min=0, max=10, step=0.01),
+           br(),
+           sliderInput(inputId="num10", label = "Granularity", 
+                       value=100, min=10, max=200)
+    ),
+    column(3, h5("Population Parameters"),
+           sliderInput(inputId="num2", label = "P(S=1)",
+                          value = 0.0275, min=0.0001, max=1),
+           sliderInput(inputId="num3", label = "P(A=1)",
+                       value = 0.15, min= 0.0001, max=1),
+           sliderInput(inputId = "num4",label = "P(Y=1)",
+                       value = 0.1, min= 0.0001, max=1)
+    ),
+    column(3, h5("Selection Effects"),
+           sliderInput(inputId = "num6", label = "b0_range",
+                          value=c(0,0.1), min=0, max=1),
+           sliderInput(inputId = "num7", label = "ba_range",
+                       value=c(-0.2,0.2), min=-0.5, max=0.5),
+           sliderInput(inputId = "num8", label = "by_range", 
+                       value=c(-0.2,0.2), min=-0.5, max=0.5)
+    )
                  
-                 
+  ),
+  
+  mainPanel(
                  
                  plotOutput("scatter")
                  )
+  )
+
 
 server <- function(input, output) {
   
@@ -43,11 +61,11 @@ server <- function(input, output) {
            pS=input$num2, 
            pA=input$num3,
            pY=input$num4,
-           pAY=input$num5,
+           pAY=0,
            b0_range=input$num6, 
            ba_range=input$num7, 
            by_range=input$num8, 
-           bay_range=input$num9, 
+           bay_range=c(0,0), 
            granularity=input$num10
       )
     x$scatter()
