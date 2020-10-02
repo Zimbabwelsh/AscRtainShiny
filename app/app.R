d6a7ced687fcd375ef4b84aa772c637e2fb0447d
Gareth <garethjgriffith1@gmail.com>
2020-10-02 17:23:31 +0100

    graph title

--- app/app.R
+++ app/app.R
@@ -264,6 +264,7 @@ server <- function(input, output) {
       z <- ggplot2::ggplot(x$param, ggplot2::aes(x=ba, y=by, label=bay)) +
               ggplot2::geom_point(ggplot2::aes(colour=b0), size=0.5) + 
               ggplot2::xlab("$\\beta_A$") + ggplot2::ylab("$\\beta_Y$") + 
+              ggplot2::ggtitle("Selection Effects giving rise to Observed OR under a true null association") +
               ggplot2::labs(colour= "b0")
       
       z <- z+geom_hline(yintercept=0,  size=0.2)
